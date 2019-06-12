25069f09662964b608373cde6209fba98a1ece8f
davidbridges123 <david.bridges@live.com>
2019-06-12 16:44:29 -0600

    adding questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-
+$(document).ready(function(){
+  
 // need start quiz funtion 
 function startQuiz() {
 
@@ -10,9 +11,42 @@ function showResults () {
 // call star quiz funtion 
 showResults ();
 // need array for questions
-var questions = {
+var questions = [
+{
+    question: "Who did the first 900?",
+    answers: {
+        a: "Tony Hawk",
+        b: "Bob Burnquist",
+        c: "Steve Caballero",
+        d: "Ryan Sheckler",
+    correctAnswer: "a",
+    }
+    },
+    { 
+        question: "What skateboard deck company does Chris Cole ride for?",
+    answers: {
+        a: "birdhouse",
+        b: "zero",
+        c: "mystery",
+        d: "baker",
+    correctAnswer: "b",
+    }
+    },
+    { 
+        question: "What year was Powell Peralta founded?",
+    answers: {
+        a: "1950",
+        b: "1973",
+        c: "1978",
+        d: "1985",
+    correctAnswer: "c",
+    }
+    },
 
-}
+
+]
+
+
 
 // need variables to store wins , losses and results
 
@@ -25,3 +59,4 @@ var submit = $("#submit")
 
 $(document).on("click", "#submit", showResults() );
 $(document).on("click", ".#startbtn", startQuiz () );
+)};
