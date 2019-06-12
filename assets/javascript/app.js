3e4b4e85e0cec8822fab573f6a6bebc7531742df
davidbridges123 <david.bridges@live.com>
2019-06-12 12:41:38 -0600

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,22 @@
 
 // need start quiz funtion 
+function startQuiz() {
 
+}
 // need results function to check for answers
+function showResults () {
 
+}
 // call star quiz funtion 
-
+showResults ();
 // need array for questions
+var questions = {
 
+}
 
 // need variables to store wins , losses and results
+
+var quizContainer = $("#quiz")
+var resultsContainer = $("#results")
+var submit = $("#submit")
 
