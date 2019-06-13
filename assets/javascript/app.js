966eda13590cbdefb91ea92aaa12fee25581f509
davidbridges123 <david.bridges@live.com>
2019-06-12 22:59:13 -0600

    restarting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,17 @@
-$(document).ready(function(){
-  
-// need start quiz funtion 
-function startQuiz() {
-
-}
-// need results function to check for answers
-function showResults () {
-
-}
-// call star quiz funtion 
-showResults ();
-// need array for questions
-var questions = [
-{
-    question: "Who did the first 900?",
-    answers: {
-        a: "Tony Hawk",
-        b: "Bob Burnquist",
-        c: "Steve Caballero",
-        d: "Ryan Sheckler",
-    correctAnswer: "a",
-    }
-    },
-    { 
-        question: "What skateboard deck company does Chris Cole ride for?",
-    answers: {
-        a: "birdhouse",
-        b: "zero",
-        c: "mystery",
-        d: "baker",
-    correctAnswer: "b",
-    }
-    },
-    { 
-        question: "What year was Powell Peralta founded?",
-    answers: {
-        a: "1950",
-        b: "1973",
-        c: "1978",
-        d: "1985",
-    correctAnswer: "c",
-    }
-    },
-    { 
-        question: "Who invented the 360 flip?",
-    answers: {
-        a: "Rodney Mullen",
-        b: "Tony Hawk",
-        c: "Daewon Song",
-        d: "Lance Mountain",
-    correctAnswer: "a",
-    }
-    },
-
-]
+var sec = 60
+var timer = setInterval(function() { 
+   $('#timer').text(sec--);
+   if (sec == -1) {
+      $('#hideMsg').fadeOut('fast');
+      clearInterval(timer);
+   } 
+}, 1000);
 
 
-
-// need variables to store wins , losses and results
-
-var quizContainer = $("#quiz")
-var resultsContainer = $("#results")
-var submit = $("#submit")
+// $(document).on("click", "#submit", showResults() );
+// $(document).on("click", ".#startbtn", startQuiz () );
 
 
 
 
-$(document).on("click", "#submit", showResults() );
-$(document).on("click", ".#startbtn", startQuiz () );
-)};
+
