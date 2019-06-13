45abb1acf20539a62ff61e967b436f2157874e75
davidbridges123 <david.bridges@live.com>
2019-06-12 23:11:51 -0600

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
-var sec = 60
-var timer = setInterval(function() { 
-   $('#timer').text(sec--);
-   if (sec == -1) {
-      $('#hideMsg').fadeOut('fast');
-      clearInterval(timer);
-   } 
-}, 1000);
+$(document).ready(function () {
+    console.log("ready!");
+
+$("#startbtn").on("click", function () {
+    startQuiz ()
+}); 
+
+    function startTimer() {
+        var sec = 60
+        var timer = setInterval(function () {
+            $('#timer').text(sec--);
+            if (sec == -1) {
+                $('#hideMsg').fadeOut('fast');
+                clearInterval(timer);
+            }
+        }, 1000);
+
+        
+    };
+    function startQuiz () {
+        startTimer();
+    };
 
 
+});
 // $(document).on("click", "#submit", showResults() );
-// $(document).on("click", ".#startbtn", startQuiz () );
+
 
 
 
